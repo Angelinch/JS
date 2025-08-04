d73d2c4c6c2600d7790dd0dabe0862f1d6324672
Angelinch <a.cherashova@gmail.com>
2025-08-04 20:40:07 +0300

    fikst

--- tale.js
+++ tale.js
@@ -1,5 +1,5 @@
 function kolobok(character) {
-  const name = character.toLowerCase();
+  const name = character.toLowerCase()
 
   const actions = {
     'дедушка': 'Я от дедушки ушёл',
@@ -8,12 +8,26 @@ function kolobok(character) {
   };
 
   if (actions.hasOwnProperty(name)) {
-    return actions[name];
+    return actions[name]
   } else {
-    return 'Колобок не знает этого персонажа';
+    return 'Колобок не знает этого персонажа'
   }
 }
-console.log(kolobok('дедушка'));
-console.log(kolobok('заяц'));
-console.log(kolobok('лиса'));
-console.log(kolobok('волк'));
+console.log(kolobok('дедушка'))
+console.log(kolobok('заяц'))
+console.log(kolobok('лиса'))
+console.log(kolobok('волк'))
+function newYear(character) {
+  const name = character.toLowerCase();
+
+  if (name === 'дед мороз') {
+    return 'Дед Мороз! Дед Мороз! Дед Мороз!'
+  } else if (name === 'снегурочка') {
+    return 'Снегурочка! Снегурочка! Снегурочка!'
+  } else {
+    return 'Новый год не узнаёт этого персонажа'
+  }
+}
+console.log(newYear('Дед Мороз'))
+console.log(newYear('Снегурочка'))
+console.log(newYear('Ёлка'))
