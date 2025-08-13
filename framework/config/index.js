89d16a8e6252967ad420cc71ae69cf6c8fe09993
Angelinch <a.cherashova@gmail.com>
2025-08-13 22:05:13 +0300

    dz

--- framework/config/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  baseURL: 'https://bookstore.demoqa.com',
-  credentials: {
-    password: 'Qwerty123!' // валидный пароль по требованиям API
-  }
-}
--- framework/fixtures/userData.js
+++
@@ -1,5 +0,0 @@
-function generateUserName() {
-  return `user_${Date.now()}`
-}
-
-module.exports = { generateUserName }
