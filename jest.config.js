af8ab49dd85fd6d300090a9bd5e06292b97ea380
Angelinch <a.cherashova@gmail.com>
2025-10-13 19:47:28 +0300

    al

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 // jest.config.js
-const config = {
- testEnvironment: 'node',
- verbose: true,
- testEnvironment: 'allure-jest/node',
- testEnvironmentOptions: {
- resultsDir: 'reports/allure-results'
- }
+module.exports = {
+  testEnvironment: 'node',
+  verbose: true,
+  testEnvironment: 'allure-jest/node',
+  testEnvironmentOptions: {
+    resultsDir: 'reports/allure-results'
+  }
 }
